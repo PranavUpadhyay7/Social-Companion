1df795171238667a5e5e9e7e59a2105160d726d9
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2025-08-13 01:20:01 +0530

    footer

--- app/clubbers/page.js
+++ app/clubbers/page.js
@@ -1,23 +1,30 @@
 "use client";
 import React from "react";
-
+import FooterDiv from "./ClubberComponents/FooterDiv";
 import FirstDiv from "./ClubberComponents/FirstDiv";
 import FeedMap from "./ClubberComponents/FeedMap";
 import ThirdDiv from "./ClubberComponents/ThirdDiv";
 
 function page() {
   return (
-    <div className="flex  md:flex-row w-[100vw] justify-between">
-      <div className=" w-1/3">
-        <FirstDiv />
+    <>
+      <div className="flex flex-col md:flex-row w-[100vw] justify-between">
+        <div className="hidden md:block md:w-1/3">
+          <FirstDiv />
+        </div>
+        <div className="pt-20 md:w-2/3 text-center mx-auto h-[100vh]">
+          <FeedMap />
+        </div>
+        <div className="hidden md:block md:pt-20 md:w-1/3">
+          <ThirdDiv />
+        </div>
       </div>
-      <div className="pt-20 w-2/3  h-[100vh]">
-        <FeedMap />
+
+      {/* Mobile footer - fixed */}
+      <div className="md:hidden">
+        <FooterDiv />
       </div>
-      <div className="pt-20 w-1/3">
-        <ThirdDiv />
-      </div>
-    </div>
+    </>
   );
 }
 export default page;
