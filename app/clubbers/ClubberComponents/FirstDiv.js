01f60385bcee5c6bbda8b4503ef75d57492c0891
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2025-08-15 14:35:21 +0530

    settings page

--- app/clubbers/ClubberComponents/FirstDiv.js
+++ app/clubbers/ClubberComponents/FirstDiv.js
@@ -18,38 +18,45 @@ function FirstDiv() {
           </Link>
         </div>
         <button className="hover:bg-zinc-800 mx-4 px-2 py-2 rounded-md">
-          <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
-            <HomeIcon className="h-6 w-6 text-white" />
-            <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
-              Home
-            </h1>
-          </div>
-        </button>
-
+          <Link href="/">
+            <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
+              <HomeIcon className="h-6 w-6 text-white" />
+              <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
+                Home
+              </h1>
+            </div>
+          </Link>
+        </button>{" "}
         <button className="hover:bg-zinc-800 mx-4 px-2 py-2 rounded-md">
-          <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
-            <ChatBubbleBottomCenterTextIcon className="h-6 w-6 text-white" />
-            <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
-              Conversation
-            </h1>
-          </div>
-        </button>
-
+          <Link href="/">
+            <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
+              <ChatBubbleBottomCenterTextIcon className="h-6 w-6 text-white" />
+              <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
+                Conversation
+              </h1>
+            </div>
+          </Link>
+        </button>{" "}
         <button className="hover:bg-zinc-800 mx-4 px-2 py-2 rounded-md">
-          <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
-            <UserGroupIcon className="h-6 w-6 text-white" />
-            <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
-              Groups
-            </h1>
-          </div>
-        </button>
+          <Link href="/">
+            <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
+              <UserGroupIcon className="h-6 w-6 text-white" />
+              <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
+                Groups
+              </h1>
+            </div>
+          </Link>
+        </button>{" "}
         <button className="hover:bg-zinc-800 mx-4 px-2 py-2 rounded-md">
-          <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
-            <Cog6ToothIcon className="h-6 w-6 text-white" />
-            <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
-              Settings
-            </h1>
-          </div>
+          <Link href="/settingsu">
+            {" "}
+            <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
+              <Cog6ToothIcon className="h-6 w-6 text-white" />
+              <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
+                Settings
+              </h1>
+            </div>
+          </Link>
         </button>
       </div>
     </div>
