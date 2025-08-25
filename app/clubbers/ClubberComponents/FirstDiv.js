87d3d014b6d3ca3e51f91c1e4c602498031098cd
PranavUpadhyay7 <pranavupadhyay10@gmail.com>
2025-08-25 21:59:41 +0530

    use state use

--- app/clubbers/ClubberComponents/FirstDiv.js
+++ app/clubbers/ClubberComponents/FirstDiv.js
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import {
   HomeIcon,
   ChatBubbleBottomCenterTextIcon,
@@ -9,6 +10,15 @@ import {
 import Link from "next/link";
 
 function FirstDiv() {
+  const [hidden, block] = useState("hidden");
+
+  function divShow() {
+    block("block");
+    console.log("Current hidden state:", hidden);
+  }
+
+  function divHide() {}
+
   return (
     <div>
       <div className="fixed flex flex-col gap-10 mx-auto text-center border-r-[1px] border-stone-800 h-[100vh] ">
@@ -57,6 +67,18 @@ function FirstDiv() {
               </h1>
             </div>
           </Link>
+        </button>
+        <button
+          className="hover:bg-zinc-800 mx-4 px-2 py-2 rounded-md"
+          onClick={divShow}
+        >
+          {" "}
+          <div className="flex justify-center xl:justify-start items-center gap-0 xl:gap-3">
+            <Cog6ToothIcon className="h-6 w-6 text-white" />
+            <h1 className="hidden xl:block xl:text-white xl:font-extrabold xl:text-md">
+              Settings
+            </h1>
+          </div>
         </button>
       </div>
     </div>
--- app/settingsu/SettingComponents/EditProfile.js
+++ app/settingsu/SettingComponents/EditProfile.js
@@ -5,41 +5,45 @@ import { XMarkIcon, HomeIcon } from "@heroicons/react/24/outline";
 
 const EditProfile = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen">
-      <XMarkIcon className="h-6 w-6 text-white absolute top-4 right-4 cursor-pointer" />
+    <div className="flex items-center justify-center h-screen w-screen bg-black/50">
+      <div className="relative flex flex-col items-center justify-center h-[70vh] w-[35vw] bg-zinc-900">
+        <XMarkIcon className="h-6 w-6 text-white absolute top-4 right-4 cursor-pointer" />
 
-      <div className="text-white text-center mb-4">Profile Picture</div>
-      <form
-        onSubmit={null}
-        className="flex flex-col gap-2 w-[30vw] mx-auto p-6 border rounded-lg"
-      >
-        <h1 className="text-white text-base">Bio</h1>
-        <input
-          type="text"
-          name="name"
-          placeholder=""
-          onChange={null}
-          className="border bg-zinc-900 hover:bg-zinc-800 p-3 mb-6 rounded-lg 
+        <div className="text-white font-bold text-center mb-4">
+          Profile Picture
+        </div>
+        <form
+          onSubmit={null}
+          className="flex flex-col gap-2 w-[30vw] mx-auto p-6  rounded-lg"
+        >
+          <h1 className="text-white font-semibold text-base">Bio</h1>
+          <input
+            type="text"
+            name="bio"
+            placeholder=""
+            onChange={null}
+            className="border bg-zinc-900 text-white text-sm hover:bg-zinc-800 border-gray-500  p-3 mb-6 rounded-lg 
              focus:outline-none focus:ring-[1px] focus:ring-[#7F00FF] 
              focus:border-[#7F00FF] transition-all duration-200  "
-        />
-        <h1 className="text-white">Gender</h1>
-        <input
-          type="email"
-          name="email"
-          placeholder=""
-          onChange={null}
-          className="border bg-zinc-900 hover:bg-zinc-800 p-3 mb-6 rounded-lg 
+          />
+          <h1 className="text-white font-semibold">Gender</h1>
+          <input
+            type=""
+            name=""
+            placeholder=""
+            onChange={null}
+            className="border bg-zinc-900 text-white hover:bg-zinc-800 p-3 mb-6 border-gray-500 rounded-lg 
              focus:outline-none focus:ring-[1px] focus:ring-[#7F00FF] 
              focus:border-[#7F00FF] transition-all duration-200"
-        />
-        <button
-          type="submit"
-          className="text-white bg-[#7F00FF] px-3 py-4 rounded-lg"
-        >
-          Update Profile
-        </button>
-      </form>
+          />
+          <button
+            type="submit"
+            className="text-white bg-[#7F00FF] px-3 py-4 rounded-lg"
+          >
+            Update Profile
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
